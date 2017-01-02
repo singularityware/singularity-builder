534f325f7466cecd0f9aba481b248b2ad677260e
vsoch <vsochat@stanford.edu>
2017-01-01 21:57:14 -0800

    updating builder to use latest, but not run command.

--- singularity-build.sh
+++ singularity-build.sh
@@ -5,11 +5,10 @@ sudo apt-get install -y build-essential libtool autotools-dev automake autoconf
 sudo apt-get install -y python3-pip
 cd /tmp && git clone http://www.github.com/singularityware/singularity
 cd singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && sudo make install
+export SINGULARITY_VERSION=$(singularity --version)
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade google-api-python-client
 sudo pip3 install --upgrade google
 sudo pip3 install oauth2client==3.0.0
 sudo pip3 install gitpython
-cd /tmp && git clone https://github.com/singularityware/singularity-python
-cd singularity-python && python3 setup.py sdist && sudo python3 setup.py install
-python3 -c "from singularity.build import run_build; run_build()" > /tmp/.shub-log 2>&1
+sudo pip3 install singularity
