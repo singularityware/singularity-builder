3a9bf2097f3d7f02a62022c210dd8a8464e4fd3b
vsoch <vsochat@stanford.edu>
2017-01-02 03:48:16 -0800

    modified:   singularity-build.sh

--- singularity-build.sh
+++ singularity-build.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install git \
 
 # Install Singularity from Github
 cd /tmp && git clone http://www.github.com/singularityware/singularity &&
-   cd singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && sudo make install
+   cd singularity && ./configure --prefix=/usr/local && make && sudo make install
 
 # Pip3 installs
 sudo pip3 install --upgrade pip &&
