e9dbf9708771f99a31b82d3ad5821b3734cf8f5e
Eduardo Arango <carlos.arango.gutierrez@correounivalle.edu.co>
2017-04-10 17:49:55 -0500

    Update singularity_build.sh
    All Comments and tips done

--- singularity_build.sh
+++ singularity_build.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 #Singularity Builder
-#Singularity: Application containers for Linux http://singularity.lbl.gov/
-# For Build + install do
-# sudo ./singularity_build.sh build install
-# If you have all that's needed simply go for
-# sudo ./singularity_build.sh install
-# To Update do
-# sudo ./singularity_build.sh update
+#Singularity: containers for Linux http://singularity.lbl.gov/
 
-if [ "$#" -lt 1 ]
+if [ "$#" -lt 1 ];
 then
-echo  "Please insert at least one argument"
+echo "Singularity Builder: build, install, and update Singularity software. 
+Support for Debian/Ubuntu, Centos/Fedora
+
+USAGE: ./singularity_builder.sh <command> [options] ...
+
+COMMANDS:
+     all        setup, build, and install [sudo]
+     setup     	install dependencies for your distribution [sudo]
+     build      configure and make the installation, with optional --prefix
+     install    make, and make install [sudo]
+     update 	[sudo]
+
+DEVELOPMENT:
+   install-devel: equivalent to install, but using development branch 
+   update-devel  equivalent to update, but using development branch
+
+Examples:
+
+  # Install dependencies, setup, build + install
+  sudo ./singularity_build.sh all
+
+  # Install dependencies, build + optional prefix
+  sudo ./singularity_build.sh build --prefix=/my/path
+ 
+  # If you already have dependencies, just install
+  sudo ./singularity_build.sh install
+
+  # Update an installation
+  sudo ./singularity_build.sh update
+
+Singularity: Application containers for Linux
+For additional help, see http://singularity.lbl.gov/"
+exit 1
 fi
 
-for arg in $@
-do
-case "$arg" in
-	"build")
-	ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+setup () {
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 	if [ -f /etc/debian_version ]; then
     	apt-get update > /tmp/.install-log
 	apt-get -y install git \
@@ -55,34 +78,112 @@ case "$arg" in
 			;;
 	esac
 	fi
+}
+
+remove () {
+rm -rf $1/libexec/singularity
+rm -rf $1/etc/singularity
+rm -rf $1/include/singularity
+rm -rf $1/lib/singularity
+rm -rf $1/var/lib/singularity/
+rm $1/bin/singularity
+rm $1/bin/run-singularity
+rm $1/etc/bash_completion.d/singularity 
+rm $1/man/man1/singularity.1
+}
+
+for arg in $@
+do
+case "$arg" in
+	"setup")
+	if [ "$(id -u)" != "0" ]; then
+	echo "Please run as root."
+	exit 1
+	else
+	setup
+	fi
+	;;
+	"build")
+# Build configure and make the installation, with optional --prefix
+	cd /tmp && git clone http://www.github.com/singularityware/singularity 
+	if [ $2 ];
+	then	
+	cd /tmp/singularity && ./autogen.sh && ./configure $2 && make
+	else	
+	cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make
+	fi	
 	;;
 	"install")	
 # Install Singularity from Github
 	cd /tmp && git clone http://www.github.com/singularityware/singularity 
+	if [ "$(id -u)" != "0" ]; then
+	echo "Please run as root."
+	exit 1
+	else	
 	cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && make install
-	echo "SingularityApp successfully installed"
+	echo "Singularity successfully installed"
+	fi
 	;;
 	"update")
 # Update Singularity from Github
-	whereis singularity >> /tmp/singularitybins.out
-	sed -e 's/singularity://g' /tmp/singularitybins.out | rm -rf
+	if [ "$(id -u)" != "0" ]; then
+	echo "Please run as root."
+	exit 1
+	else
+		if [ $2 ];
+		then
+		remove $2
+		else
+		remove /usr/local
+		fi
 	cd /tmp && git clone http://www.github.com/singularityware/singularity 
 	cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && make install
-	echo "SingularityApp successfully installed"
+	echo "Singularity successfully installed"
+	fi
 	;;
 	"install-devel")	
 # Install Singularity-Development branch from Github
 	cd /tmp && git clone -b development http://www.github.com/singularityware/singularity 
+	if [ "$(id -u)" != "0" ]; then
+	echo "Please run as root."
+	exit 1
+	else	
 	cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && make install
-	echo "SingularityApp successfully installed"	
+	echo "Singularity successfully installed"
+	fi	
 	;;
 	"update-devel")
 # Update Singularity-Development branch from Github
-	whereis singularity >> /tmp/singularitybins.out
-	sed -e 's/singularity://g' /tmp/singularitybins.out | rm -rf
+	if [ "$(id -u)" != "0" ]; then
+	echo "Please run as root."
+	exit 1
+	else
+		if [ $2 ];
+		then
+		remove $2
+		else
+		remove /usr/local
+		fi
 	cd /tmp && git clone -b development http://www.github.com/singularityware/singularity
 	cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && make install
-	echo "SingularityApp successfully installed"
+	echo "Singularity successfully installed"
+	fi
 	;;
+	"all")
+# All setup, build, and install [sudo]
+	if [ "$(id -u)" != "0" ]; then
+	echo "Please run as root."
+	exit 1
+	else	
+	setup
+	cd /tmp && git clone http://www.github.com/singularityware/singularity 
+	cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && make install
+	echo "Singularity successfully installed"
+	fi
+	;;
+	"test")
+	remove /usr/local	
+	;;
+
 esac
 done
